d199bb563b12ff29794411b3b9a7da40cc2bc0ec
Md Irfan <mdirfancse2023@gmail.com>
2026-07-19 12:23:58 +0530

    feat: change brand text in navbar to Virtual Gyans | Md Irfan

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header({ activeTab, setActiveTab }) {
             alt="Virtual Gyans Logo" 
             style={{ width: '32px', height: '32px', objectFit: 'contain' }} 
           />
-          Virtual Gyans
+          Virtual Gyans | Md Irfan
         </a>
         
         <ul className="nav-links">
